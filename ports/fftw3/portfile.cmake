a3bbeebabde6a61bad235f86880e4c048fdd664e
Robert Schumacher <roschuma@microsoft.com>
2018-10-12 09:51:35 -0700

    [fftw3] Modernize

--- ports/fftw3/portfile.cmake
+++ ports/fftw3/portfile.cmake
@@ -1,26 +1,18 @@
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/fftw-3.3.7)
 
-# This can be removed in the next source code update
-if(EXISTS "${SOURCE_PATH}/CMakeLists.txt")
-    file(READ "${SOURCE_PATH}/CMakeLists.txt" _contents)
-    if("${_contents}" MATCHES "-D_OPENMP -DLIBFFTWF33_EXPORTS /openmp /bigobj")
-        file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/src)
-    endif()
-endif()
+set(FFTW_VERSION 3.3.7)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.fftw.org/fftw-3.3.7.tar.gz"
-    FILENAME "fftw-3.3.7.tar.gz"
+    URLS "http://www.fftw.org/fftw-${FFTW_VERSION}.tar.gz"
+    FILENAME "fftw-${FFTW_VERSION}.tar.gz"
     SHA512 a5db54293a6d711408bed5894766437eee920be015ad27023c7a91d4581e2ff5b96e3db0201e6eaccf7b064c4d32db1a2a8fab3e6813e524b4743ddd6216ba77
 )
 
-vcpkg_extract_source_archive(${ARCHIVE})
-
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/omp_test.patch
+vcpkg_extract_source_archive_ex(
+	OUT_SOURCE_PATH SOURCE_PATH
+	ARCHIVE ${ARCHIVE}
+	REF ${FFTW_VERSION}
+	PATCHES omp_test.patch
 )
 
 if ("openmp" IN_LIST FEATURES)
@@ -45,18 +37,18 @@ foreach(PRECISION ENABLE_DEFAULT_PRECISION ENABLE_FLOAT ENABLE_LONG_DOUBLE)
 
 	vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
 
-	if (VCPKG_CRT_LINKAGE STREQUAL dynamic)
+	if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
 		vcpkg_apply_patches(
-			   SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
-			   PATCHES
-					   ${CMAKE_CURRENT_LIST_DIR}/fix-dynamic.patch)
+			SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
+			PATCHES fix-dynamic.patch
+		)
 	endif()
 
 	# Cleanup
 	file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 	file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 endforeach()
-	
+
 # Handle copyright
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/fftw3)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/fftw3/COPYING ${CURRENT_PACKAGES_DIR}/share/fftw3/copyright)
