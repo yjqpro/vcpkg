ef00aac3342bd58abdab0dac3d9b64d4d210e844
Robert Schumacher <roschuma@microsoft.com>
2018-11-01 15:12:07 -0700

    [opencv] Fix targets hardcoding debug dependencies

--- ports/opencv/portfile.cmake
+++ ports/opencv/portfile.cmake
@@ -8,10 +8,6 @@ vcpkg_from_github(
     REF ${OPENCV_PORT_VERSION}
     SHA512 e1fc14285090c6fe9e26e721f2d67d7096650c523147e925567426ef76aa7f4c6f12035d6f6ce3ec7991a75a6828a810fd4f9b75f78ed5fcccecefbadd79944b
     HEAD_REF master
-)
-
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
     PATCHES
       "${CMAKE_CURRENT_LIST_DIR}/0001-winrt-fixes.patch"
       "${CMAKE_CURRENT_LIST_DIR}/0002-install-options.patch"
@@ -19,8 +15,6 @@ vcpkg_apply_patches(
       "${CMAKE_CURRENT_LIST_DIR}/0004-use-find-package-required.patch"
       "${CMAKE_CURRENT_LIST_DIR}/0005-remove-protobuf-target.patch"
 )
-
-file(WRITE "${CURRENT_BUILDTREES_DIR}/src/opencv-${OPENCV_PORT_VERSION}/rework.stamp")
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" BUILD_WITH_STATIC_CRT)
 
@@ -374,7 +368,10 @@ string(REPLACE "PREFIX}/bin"
                "PREFIX}/debug/bin" OPENCV_CONFIG_LIB "${OPENCV_CONFIG_LIB}")
 file(WRITE ${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules-debug.cmake "${OPENCV_CONFIG_LIB}")
 
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/share/opencv/OpenCVModules.cmake ${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules.cmake)
+file(READ ${CURRENT_PACKAGES_DIR}/debug/share/opencv/OpenCVModules.cmake _contents)
+string(REPLACE "${CURRENT_INSTALLED_DIR}" "\${_IMPORT_PREFIX}" _contents "${_contents}")
+string(REPLACE ";\${_IMPORT_PREFIX}/debug/" ";\${_IMPORT_PREFIX}/\\\$<\\\$<CONFIG:DEBUG>:debug/>" _contents "${_contents}")
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules.cmake "${_contents}")
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
